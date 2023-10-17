aa70bc5957be05113d08c0c54d0f9cfd5478e022
HongVy <92348180+HongVyy@users.noreply.github.com>
2023-10-17 12:49:53 -0700

    Add files via upload

--- Comparison/comparison.java
+++ Comparison/comparison.java
@@ -1,1 +1,36 @@
+//comparision examples
 
+// == equal to
+// != not equal to
+// > greater
+// < smaller
+// >= greater than or equal to
+// <= smaller than or equal to
+
+import java.util.Scanner;
+public class comparison {
+    public static void main(String[] args){
+        try (Scanner input = new Scanner(System.in)) {
+            int number1; 
+            int number2; 
+            System.out.print("Enter first number: ");
+            number1 = input.nextInt();
+            System.out.print("Enter second number: ");
+            number2 = input.nextInt();
+
+            if (number1 == number2)
+                System.out.printf("%d == %d\n", number1, number2);
+            if (number1 != number2)
+                System.out.printf("%d != %d\n", number1, number2);
+            if (number1 < number2)
+                System.out.printf("%d < %d\n", number1, number2);
+            if (number1 > number2)
+                System.out.printf("%d > %d\n", number1, number2);
+            if (number1 <= number2)
+                System.out.printf("%d <= %d\n", number1, number2);
+            if (number1 >= number2)
+                System.out.printf("%d >= %d\n", number1, number2);
+        }
+        
+    }
+}
